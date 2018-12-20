a01c5e6ed79e3f7862ef9309aafd0f397b745a42
wu <807093337@qq.com>
2018-12-20 19:32:58 +0800

    modify LoginUI and Register UI

---
+++ app/src/main/java/com/caicai/web/app/ui/activity/FindPassWordActivity.java
@@ -0,0 +1,16 @@
+package com.caicai.web.app.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.caicai.web.app.BaseActivity;
+import com.caicai.web.app.R;
+
+public class FindPassWordActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pass_word);
+    }
+
+}
